18683a8c3a82c7be164ed325a07ed2f69ba697a5
James Landrum <thyme.cypher@gmail.com>
2015-03-18 18:34:56 -0400

    Compatibility

--- backcompat.php
+++ backcompat.php
@@ -1,6 +1,6 @@
 <?php
 if (!method_exists($response,'http_response_code')) {
-  public function http_response_code() {
+  function http_response_code() {
     foreach ($this->headers as $header) {
       if (preg_match("#^HTTP/\S+\s+(\d\d\d)#i",$header,$matches)) {
         return $matches[1];
